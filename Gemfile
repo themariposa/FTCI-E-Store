a6f5bcdecdd70476ef8ec87c391c0bdd189dc529
Kelly <themariposa@gmail.com>
2019-11-20 09:31:34 -0500

    added bootstrap, setup carrierwave, setup new/edit product forms

--- Gemfile
+++ Gemfile
@@ -28,6 +28,16 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+gem 'font-awesome-rails'
+
+gem 'devise'
+gem 'carrierwave'
+gem 'cancancan'
+
+gem 'hirb'
+
+gem 'better_errors', group: :development
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_20_133547) do
+ActiveRecord::Schema.define(version: 2019_11_20_141207) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_11_20_133547) do
     t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
 end
