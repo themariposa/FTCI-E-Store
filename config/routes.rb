3f7e177c2c38ada0b01a8dfee6591b18d1098e22
Kelly <themariposa@gmail.com>
2019-11-21 10:21:40 -0500

    started building out cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  get 'cart/add_to_cart'
-  get 'cart/view_order'
-  get 'cart/checkout'
   devise_for :users
   root 'storefront#all_items'
 
   get 'categorical' => 'storefront#items_by_category'
   get 'branding' => 'storefront#items_by_brand'
+
+  post 'add_to_cart' => 'cart#add_to_cart'
+
+  get 'view_order' => 'cart#view_order'
+
+  get 'checkout' => 'cart#checkout'
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
