fedee34aecbf701174d728a4df613e150d678244
Kelly <themariposa@gmail.com>
2019-11-21 09:40:41 -0500

    built out storefront views, started on cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+	before_action :categories, :brands
+
+	def categories
+		@categories = Category.order(:name)
+	end
+
+	def brands
+		@brands = Product.pluck(:brand).sort.uniq		
+	end
 end
