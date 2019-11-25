965ac6c348feac87baad962c2ccc0bf29c8bbc7c
Kelly <themariposa@gmail.com>
2019-11-25 10:56:12 -0500

    configured strip

--- Gemfile
+++ Gemfile
@@ -38,6 +38,10 @@ gem 'hirb'
 
 gem 'better_errors', group: :development
 
+gem 'stripe', :git => 'https://github.com/stripe/stripe-ruby'
+gem 'certified'
+gem 'figaro'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,17 @@ Rails.application.routes.draw do
   root 'storefront#all_items'
 
   get 'categorical' => 'storefront#items_by_category'
-  get 'branding' => 'storefront#items_by_brand'
 
-  post 'add_to_cart' => 'cart#add_to_cart'
+  get 'branding' => 'storefront#items_by_brand'
 
   get 'view_order' => 'cart#view_order'
 
   get 'checkout' => 'cart#checkout'
+
+  post 'add_to_cart' => 'cart#add_to_cart'
+
+  post 'order_complete' => 'cart#order_complete'
+
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
